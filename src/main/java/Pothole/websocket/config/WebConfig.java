7b0cb8ed1fc3374dd021e05f81d10f23a80ce2e1
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-04 22:08:21 +0900

    ✨ [FEAT] cors 추가

--- src/main/java/Pothole/websocket/config/WebConfig.java
+++ src/main/java/Pothole/websocket/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "https://8ed6-203-252-223-253.ngrok-free.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

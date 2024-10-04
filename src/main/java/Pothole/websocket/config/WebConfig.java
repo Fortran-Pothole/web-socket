e9ea27b866be8a5ff938caa2242bac7b32951339
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-04 22:31:27 +0900

    :bug: [FIX] ì cors 주소 추가

--- src/main/java/Pothole/websocket/config/WebConfig.java
+++ src/main/java/Pothole/websocket/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:8000", "https://werewolf-happy-awfully.ngrok-free.app", "http://localhost:3000")  // 명시적인 출처 설정
+                    .allowedOriginPatterns("*")
+                    //.allowedOrigins("http://localhost:8000", "https://werewolf-happy-awfully.ngrok-free.app", "http://localhost:3000")  // 명시적인 출처 설정
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);  // 자격 증명 허용

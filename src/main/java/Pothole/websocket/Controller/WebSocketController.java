bc08e87c566132af91e96187c9b5cd36d0d0dc90
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-05 04:34:12 +0900

    active mq

--- src/main/java/Pothole/websocket/Controller/WebSocketController.java
+++ src/main/java/Pothole/websocket/Controller/WebSocketController.java
@@ -3,6 +3,7 @@ package Pothole.websocket.Controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -17,5 +18,11 @@ public class WebSocketController {
     public void sendGyroData(String message) {
         System.out.println("Received message from client: " + message); // 받은 값을 출력
         messagingTemplate.convertAndSend("/topic/km-h", message); // 받은 메시지를 클라이언트로 전송
+    }
+
+    @SubscribeMapping("/topic/gyroData")
+    public void receiveGyroData(String message) {
+        System.out.println("Received from ActiveMQ: " + message);
+        // 원하는 추가 처리 수행
     }
 }

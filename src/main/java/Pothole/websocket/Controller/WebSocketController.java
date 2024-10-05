473947f4c3a96c2fe33feb72feafd2bf1201f4f3
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-05 16:46:47 +0900

    :bug: [FIX] 메시지 형식 수정

--- src/main/java/Pothole/websocket/Controller/WebSocketController.java
+++ src/main/java/Pothole/websocket/Controller/WebSocketController.java
@@ -3,7 +3,6 @@ package Pothole.websocket.Controller;
 import Pothole.websocket.entity.VelocityData;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -26,7 +25,7 @@ public class WebSocketController {
 //        return message; // 메시지를 다시 주제로 전송
 //    }
 
-    @JmsListener(destination = "/app/sendKmH") // 클라이언트가 /app/sendKmH로 메시지를 보내면 해당 메서드가 호출됨.
+    @MessageMapping("/app/sendKmH") // 클라이언트가 /app/sendKmH로 메시지를 보내면 해당 메서드가 호출됨.
     public void sendGyroData(String message) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         VelocityData data = objectMapper.readValue(message, VelocityData.class);

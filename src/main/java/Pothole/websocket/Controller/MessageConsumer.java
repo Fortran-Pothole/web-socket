2416d0c5ffa6d88dca45f72657a3ba767b924949
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-05 16:56:07 +0900

    :bug: [FIX] 메시지 형식 수정

--- src/main/java/Pothole/websocket/Controller/MessageConsumer.java
+++ src/main/java/Pothole/websocket/Controller/MessageConsumer.java
@@ -21,7 +21,7 @@ public class MessageConsumer {
             // 메시지를 JSON으로 파싱
             ObjectMapper objectMapper = new ObjectMapper();
             VelocityData data = objectMapper.readValue(message, VelocityData.class);
-            System.out.println("JMS Received message: " + data);
+            System.out.println("JMS Received velocity: " + data.getVelocity());
         } catch (JsonProcessingException e) {
             System.err.println("JMS JSON 파싱 오류: " + e.getMessage());
             System.out.println("JMS Received raw message: " + message);

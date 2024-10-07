48337d6ed439749b362ccad8bef6ab500c4149ce
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-07 22:18:24 +0900

    [FIX] websocket stomp 제외

--- src/main/java/Pothole/websocket/exception/WebSocketHandler.java
+++ src/main/java/Pothole/websocket/exception/WebSocketHandler.java
@@ -1,11 +1,11 @@
+package Pothole.websocket.exception;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
 import java.util.ArrayList;
 import java.util.List;
-
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
